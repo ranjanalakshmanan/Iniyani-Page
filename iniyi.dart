6d4d34a0263363dd3fda14560cea683cfda8c7ad
ranjanalakshmanan <138858335+ranjanalakshmanan@users.noreply.github.com>
2023-07-07 14:17:01 +0530

    Add files via upload

---
+++ iniyi.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ranju extends StatelessWidget {
+  const ranju({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("iiiiii"),
+      ),
+    );
+  }
+}
+
+
+    
+
+
+
+
+
+
---
+++ keerthi.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class abi extends StatelessWidget {
+  const abi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text("Abinaiya's App", style: TextStyle(color: Colors.limeAccent, fontSize: 40),),
+        actions: [
+
+        ],
+      ),
+
+    );
+  }
+}
